f9d1454c9ac89f99f0a3ba3b24fb81a4e9273022
wyy <31383815119@qq.com>
2020-12-27 22:52:31 +0800

    second

---
+++ src/main/java/com/wyy/bao2/School.java
@@ -0,0 +1,21 @@
+package com.wyy.bao2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class School {
+    @Value("YIZHONG")
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
